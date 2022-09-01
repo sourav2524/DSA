b8ddb335cfba174b334db91b1d3286ba04d9fd82
sourav2524 <souravmondal2003m@gmail.com>
2022-09-01 09:41:20 +0530

    ab

--- Dsa.c
+++ Dsa.c
@@ -1,69 +1,82 @@
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 typedef struct st
 {
     int data;
     struct st* next;
 }node;
 
-
-node*create_node(int value)
-
+node* create_node(int value)
 {
-    node*temp;
-
-    temp=(node*)malloc(sizeof(node));
-    temp->data=value;
-    temp->next=0;
-
+    node* temp;
+    temp = (node*)malloc(sizeof(node));
+    temp->data = value;
+    temp->next = NULL;
     return temp;
-
 }
-
-void append(node**head,int value)
+void append(node** head,int value)
 {
-    node*temp;
-    temp=create_node(value);
-    node*current=*head;
-    if(*head==0)
+    node* temp;
+    temp = create_node(value);
+    if(*head == NULL)
     {
-        *head=temp;
+       * head = temp;
         return;
     }
-
-    while(current->next!=0)
+    node* current = *head;
+    while(current->next != NULL)
     {
-        current=current->next;
+        current = current->next;
     }
-    current->next=temp;
+    current->next = temp;
+    
 }
-
-void traverse(node*head)
+void add_node(int pos,int value, node**head)
 {
-    while(head->next!=0)
+    pos = pos-1;
+    node* newnode = create_node(value);
+    node* current = *head;
+    while(pos--)
     {
-printf("%d",head->data);
-printf("\n");
-head=head->next;
-
-
+        current = current->next;
     }
-    printf("%d\n",head->data); 
+    node* temp = current->next;
+    current->next = newnode;
+    newnode->next = temp;
+    
+}
+void delete_node(int pos, node** head)
+{
+    pos = pos-1;
+    node* current =* head;
+    while(pos--)
+    {
+        current = current->next;
     }
-
+    node* temp = current->next->next;
+    current->next = temp;
+}
+void traverse(node* head)
+{
+    while(head != NULL)
+    {
+        printf("%d ",head->data);
+        head = head->next;
+    }
+}
 int main()
-{  
-append(&head,1);
-append(&head,2);
-append(&head,3);
-append(&head,4);
-append(&head,5);
-append(&head,6);
-
-printf("original\n");
-traverse(head);
-
-
-
+{
+    node* head = NULL;
+    append(&head, 1);
+    append(&head, 2);
+    append(&head, 3);
+    append(&head, 4);
+    traverse(head);
+    printf("\n");
+    add_node(2,5,&head);
+    traverse(head);
+    printf("\n");
+    delete_node(2,&head);
+    traverse(head);
+    return 0;
 }
---
+++ gold.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int G,S,A,B;
+
+
+    
+    scanf("%d%d%d%d",&G,&S,&A,&B);
+
+
+
+    if(G>S )
+    {
+        printf("Gold");
+
+    }
+    else
+    {
+        printf("silver");
+    }
+    return 0;
+}
